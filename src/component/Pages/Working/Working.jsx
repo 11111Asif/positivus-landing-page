c82892be7717867360597b53da81a733e7bc81b0
11111Asif <asif040502@gmail.com>
2025-08-05 14:19:20 -0700

    added to working section

--- src/component/Pages/Working/Working.jsx
+++ src/component/Pages/Working/Working.jsx
@@ -3,11 +3,69 @@
 const Working = () => {
     return (
         <div className="lg:px-24 px-4 py-5 bg-white">
-            <div className="flex gap-4">
-                <h1 className="text-4xl rounded-2xl w-[398px] text-black text-center h-[51px] bg-[#B9FF66]">Our Working Process </h1>
+            <div className="lg:flex gap-4">
+                <h1 className="lg:text-4xl text-3xl rounded-2xl lg:w-[398px] w-[340px] text-black text-center h-[51px] bg-[#B9FF66]">Our Working Process </h1>
                 <p className="text-black">
-                    Step-by-Step Guide to Achieving<br/> Your Business Goals
+                    Step-by-Step Guide to Achieving<br /> Your Business Goals
                 </p>
+            </div>
+            <div className="mt-10">
+                <div className="collapse collapse-plus bg-[#B9FF66] w-auto lg:h-[159px] h-[250px] border border-b-4 rounded-3xl border-black">
+                    <input type="radio" name="my-accordion-3" defaultChecked />
+                    
+                   <div className="collapse-title text-3xl font-medium text-black"><span className="text-4xl text-black font-bold mr-4">01</span>Consultation</div>
+                    <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="collapse collapse-plus bg-[#F3F3F3] border border-b-4 border-black w-auto h-[320px] lg:h-[159px] mt-5 rounded-3xl">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-3xl text-black font-medium"><span className="text-4xl text-black font-bold mr-4">02</span>Research and Strategy Development</div>
+                    <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#F3F3F3] border border-b-4 border-black w-auto h-[250px] lg:h-[159px] mt-5 rounded-3xl">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-3xl text-black font-medium"><span className="text-4xl text-black font-bold mr-4">03</span>Implementation</div>
+                     <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#F3F3F3] border border-b-4 border-black w-auto h-[300px] lg:h-[159px] mt-5 rounded-3xl">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-3xl text-black font-medium"><span className="text-4xl text-black font-bold mr-4">04</span>Monitoring and Optimization</div>
+                     <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#F3F3F3] border border-b-4 border-black w-auto h-[300px] lg:h-[159px] mt-5 rounded-3xl">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-3xl text-black font-medium"><span className="text-4xl text-black font-bold mr-4">05</span>Reporting and Communication</div>
+                     <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="collapse collapse-plus bg-[#F3F3F3] border border-b-4 border-black w-auto h-[159px] mt-5 rounded-3xl">
+                    <input type="radio" name="my-accordion-3" />
+                    <div className="collapse-title text-3xl text-black font-medium"><span className="text-4xl text-black font-bold mr-4">06</span>Continual Improvement</div>
+                     <div className="collapse-content ">
+                        <div className="border-t-2 border-black">
+                            <p className="text-black py-3">During the initial consultation, we will discuss your business goals and objectives, target audience, and current marketing efforts. This will allow us to understand your needs and tailor our services to best fit your requirements.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
